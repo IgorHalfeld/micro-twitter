23e9b781e074fe8a2830bb5d9aad8328c46a4e8e
Igor Halfeld <hello@igorluiz.me>
2019-08-03 20:31:50 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,65 @@
+(() => {
+  'use strict';
+
+  const $ = element => document.querySelector(element);
+
+  const loadComponent = async component => {
+    const res = await fetch(`/components/${component}.html`);
+    return await res.text();
+  };
+
+  const attachTweetToDom = tweetComponent => {
+    const div = document.createElement('div');
+    div.innerHTML = tweetComponent;
+    $('#content').appendChild(div);
+  };
+
+  const parseDate = dateString => {
+    const date = new Date(dateString);
+    return `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()} ${date.toLocaleTimeString()}`
+  };
+
+  const cleanUpTweetInput = () => {
+    const tweetText = $('#tweetText');
+    tweetText.value = '';
+    tweetText.blur();
+  };
+
+  const getValues = () => ({
+    tweet: $('#tweetText').value,
+  });
+
+  const toggleDisableButton = () => {
+    const { tweet } = getValues();
+    (tweet && tweet.length)
+      ? $('#tweetButton').disabled = ''
+      : $('#tweetButton').disabled = 'disabled';
+  };
+
+  const publishTweet = async () => {
+    const {
+      tweet,
+      publishAt = new Date().toISOString(),
+    } = getValues();
+    const widget = await loadComponent('widget');
+    const tweetComponent = widget
+      .replace('{{text}}', tweet)
+      .replace('{{date}}', parseDate(publishAt));
+    
+    attachTweetToDom(tweetComponent);
+    cleanUpTweetInput();
+  };
+
+  const addEventListeners = () => {
+    const tweetText = $('#tweetText');
+    const tweetButton = $('#tweetButton');
+
+    tweetText.addEventListener('keyup', toggleDisableButton);
+    tweetButton.addEventListener('click', publishTweet);
+
+    tweetText.focus();
+  };
+  
+  addEventListeners();
+  toggleDisableButton();
+})();
